807d1fef65bed9f8de1de1d6a0e04844473e983d
Alissa <99267320+alissarm@users.noreply.github.com>
2022-06-10 15:14:16 -0300

    Add files via upload

---
+++ Formulário de satisfação do cliente/script.js
@@ -0,0 +1,153 @@
+let botao_enviar = document.querySelector('#btn-enviar')
+ 
+botao_enviar.addEventListener('click', enviarResposta)
+ 
+function enviarResposta(){
+    let q1 = document.querySelector('#q1_1')
+    let q2 = document.querySelector('#q1_2')
+    let q3 = document.querySelector('#q1_3')
+    let q4 = document.querySelector('#q1_4')
+    let q5 = document.querySelector('#q1_5')
+}
+
+function enviarResposta(){
+    let q1 = document.querySelector('#q2_1')
+    let q2 = document.querySelector('#q2_2')
+    let q3 = document.querySelector('#q2_3')
+    let q4 = document.querySelector('#q2_4')
+    let q5 = document.querySelector('#q2_5')
+}
+function enviarResposta(){
+    let q1 = document.querySelector('#q3_1')
+    let q2 = document.querySelector('#q3_2')
+    let q3 = document.querySelector('#q3_3')
+    let q4 = document.querySelector('#q3_4')
+    let q5 = document.querySelector('#q3_5')
+}
+function enviarResposta(){
+    let q1 = document.querySelector('#q4_1')
+    let q2 = document.querySelector('#q4_2')
+    let q3 = document.querySelector('#q4_3')
+    let q4 = document.querySelector('#q4_4')
+    let q5 = document.querySelector('#q4_5')
+}
+function enviarResposta(){
+    let q1 = document.querySelector('#q5_1')
+    let q2 = document.querySelector('#q5_2')
+    let q3 = document.querySelector('#q5_3')
+    let q4 = document.querySelector('#q5_4')
+    let q5 = document.querySelector('#q5_5')
+}
+function enviarResposta(){
+    let q1 = document.querySelector('#q6_1')
+    let q2 = document.querySelector('#q6_2')
+    let q3 = document.querySelector('#q6_3')
+    let q4 = document.querySelector('#q6_4')
+    let q5 = document.querySelector('#q6_5')
+}
+function enviarResposta(){
+    let q1 = document.querySelector('#q7_1')
+    let q2 = document.querySelector('#q7_2')
+    let q3 = document.querySelector('#q7_3')
+    let q4 = document.querySelector('#q7_4')
+    let q5 = document.querySelector('#q7_5')
+    let q6 = document.querySelector('#q7_6')
+}
+function enviarResposta(){
+    let q1 = document.querySelector('#q8_1')
+    let q2 = document.querySelector('#q8_2')
+    let q3 = document.querySelector('#q8_3')
+    let q4 = document.querySelector('#q8_4')
+    let q5 = document.querySelector('#q8_5')
+}
+
+if (q1_1.checked){
+    alert('Obrigado Pela boa avaliaçao!')
+}else if (q1_2.checked){
+alert('Obrigado Pela avaliaçao!')
+}else if (q1_3.checked){
+alert('Obrigado!')
+}else if (q1_4.checked){
+alert('Puxa, que coisa!')
+}else if (q1_5.checked)
+alert('Sentimos muito pelo ocorrido')
+
+if (q2_1.checked){
+    alert('Obrigado Pela boa avaliaçao!')
+}else if (q2_2.checked){
+alert('Obrigado Pela avaliaçao!')
+}else if (q2_3.checked){
+alert('Obrigado!')
+}else if (q2_4.checked){
+alert('Puxa, que coisa!')
+}else if (q2_5.checked)
+alert('Sentimos muito pelo ocorrido')
+
+if (q3_1.checked){
+    alert('Obrigado Pela boa avaliaçao!')
+}else if (q3_2.checked){
+alert('Obrigado Pela avaliaçao!')
+}else if (q3_3.checked){
+alert('Obrigado!')
+}else if (q3_4.checked){
+alert('Puxa, que coisa!')
+}else if (q3_5.checked)
+alert('Sentimos muito pelo ocorrido')
+
+if (q4_1.checked){
+    alert('Obrigado Pela boa avaliaçao!')
+}else if (q4_2.checked){
+alert('Obrigado Pela avaliaçao!')
+}else if (q4_3.checked){
+alert('Obrigado!')
+}else if (q4_4.checked){
+alert('Puxa, que coisa!')
+}else if (q4_5.checked)
+alert('Sentimos muito pelo ocorrido')
+
+if (q5_1.checked){
+    alert('Obrigado Pela boa avaliaçao!')
+}else if (q5_2.checked){
+alert('Obrigado Pela avaliaçao!')
+}else if (q5_3.checked){
+alert('Obrigado!')
+}else if (q5_4.checked){
+alert('Puxa, que coisa!')
+}else if (q5_5.checked)
+alert('Sentimos muito pelo ocorrido')
+
+if (q6_1.checked){
+    alert('Obrigado Pela boa avaliaçao!')
+}else if (q6_2.checked){
+alert('Obrigado Pela avaliaçao!')
+}else if (q6_3.checked){
+alert('Obrigado!')
+}else if (q6_4.checked){
+alert('Puxa, que coisa!')
+}else if (q6_5.checked)
+alert('Sentimos muito pelo ocorrido')
+
+if (q7_1.checked){
+    alert('Obrigado Pela boa avaliaçao!')
+}else if (q7_2.checked){
+alert('Obrigado Pela avaliaçao!')
+}else if (q7_3.checked){
+alert('Obrigado!')
+}else if (q7_4.checked){
+alert('Obrigado!')
+}else if (q7_5.checked){
+alert('Puxa que coisa')
+}else if (q7_6.checked)
+alert('Que bom :)')
+
+if (q8_1.checked){
+    alert('Obrigado Pela boa avaliaçao!')
+}else if (q8_2.checked){
+alert('Obrigado Pela avaliaçao!')
+}else if (q8_3.checked){
+alert('Obrigado!')
+}else if (q8_4.checked){
+alert('Puxa, que coisa!')
+}else if (q8_5.checked)
+alert('Sentimos muito pelo ocorrido')
+
