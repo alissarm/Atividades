c5b040e0c7ec9583bf09ecfac1f3c16b594fe00a
Alissa <99267320+alissarm@users.noreply.github.com>
2023-06-27 10:06:16 -0300

    Add files via upload

---
+++ busca de githubs/script.js
@@ -0,0 +1,20 @@
+function getProfile(){
+    let name = document.getElementById('name')
+    let public_repos = document.getElementById('public_repos')
+    let followers = document.getElementById('followers')
+    let following = document.getElementById('following')
+    let url = document.getElementById('url')
+
+    const username = document.getElementById("username").value;
+    fetch("https:/api.github.com/users/" + username)
+    .then(res => res.json())
+    .then(data => {
+
+        name.value = data.name
+        login.value = data.login
+        public_repos.value = data.public_repos
+        followers.value = data.followers
+        following.value = data.following
+        url.value = data.url
+    })
+}
